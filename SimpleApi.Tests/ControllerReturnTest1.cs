cc6ea0057f540b34a97c174fea21d01462d4b0c2
isaacMwesigwa <mwesigwai433@gmail.com>
2024-06-12 08:37:33 +0300

    Add project files.

---
+++ SimpleApi.Tests/ControllerReturnTest1.cs
@@ -0,0 +1,21 @@
+﻿using SimpleApi.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleApi.Tests
+{
+    public class ControllerReturnTest1
+    {
+        [Fact]
+        public void ValuesController_Get_returns_correctName_when_id_is_provided()
+        {
+            var controller = new ValuesController();
+            var controllerReturn = controller.Get(1).Value;
+            Assert.NotNull(controllerReturn);
+            Assert.Equal("Isaac Mwesigwa", controllerReturn);
+        }
+    }
+}
---
+++ SimpleApi/Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            var testResult = new List<string>
+            {
+                "Isaac",
+                "Mwesigwa"
+            };
+            return Ok(testResult);
+        }
+
+
+        [HttpGet("{id : int}")]
+        public ActionResult<string> Get(int id)
+        {
+            //for test purposes is redundant
+            return "Isaac Mwesigwa";
+        }
+    }
+}
